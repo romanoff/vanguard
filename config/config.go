d1c06fc5fd5c6f7bfe24472a238bfb0602d4167b
Andriy Romanov <judo.ras@gmail.com>
2014-09-18 12:14:58 -0700

    Separation of vanguard config into tiers

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"gopkg.in/yaml.v1"
 	"io/ioutil"
 )
@@ -8,6 +9,41 @@ import (
 type Config struct {
 	Application string
 	Servers     []*Server
+}
+
+func (self *Config) GetTiers() ([]*Tier, error) {
+	containersCount := 0
+	for _, server := range self.Servers {
+		containersCount += len(server.Containers)
+	}
+	launchedContainers := []string{}
+	tiersContainersCount := 0
+	tiers := []*Tier{}
+	for {
+		tier := &Tier{}
+		tierLaunchedContainers := []string{}
+		for _, server := range self.Servers {
+			for _, container := range server.Containers {
+				if NotIn(launchedContainers, container.Name()) &&
+					container.CanBeLaunchedWith(launchedContainers) {
+					tiersContainersCount++
+					tier.AddContainer(server, container)
+					if NotIn(tierLaunchedContainers, container.Name()) {
+						tierLaunchedContainers = append(tierLaunchedContainers, container.Name())
+					}
+				}
+			}
+		}
+		tiers = append(tiers, tier)
+		launchedContainers = append(launchedContainers, tierLaunchedContainers...)
+		if tiersContainersCount == containersCount {
+			break
+		}
+		if len(tier.Servers) == 0 {
+			return nil, errors.New("circular links dependency in vanguard.yml")
+		}
+	}
+	return tiers, nil
 }
 
 type Server struct {
@@ -22,6 +58,49 @@ type Container struct {
 	Links []string `yml:"links,omitempty"`
 }
 
+func (self *Container) String() string {
+	return self.Name()
+}
+
+func (self *Container) Name() string {
+	name := self.Image
+	if self.Tag != "" {
+		name += ":" + self.Tag
+	}
+	return name
+}
+
+func (self *Container) CanBeLaunchedWith(names []string) bool {
+	var dependencyMissing bool
+	for _, link := range self.Links {
+		dependencyMissing = true
+		for _, name := range names {
+			if name == link {
+				dependencyMissing = false
+			}
+		}
+	}
+	return !dependencyMissing
+}
+
+type Tier struct {
+	Servers []*Server
+}
+
+func (self *Tier) AddContainer(server *Server, container *Container) {
+	var usedServer *Server
+	for _, s := range self.Servers {
+		if s.Hostname == server.Hostname {
+			usedServer = s
+		}
+	}
+	if usedServer == nil {
+		usedServer = &Server{Hostname: server.Hostname}
+		self.Servers = append(self.Servers, usedServer)
+	}
+	usedServer.Containers = append(usedServer.Containers, container)
+}
+
 func ParseConfig(path string) (*Config, error) {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -30,4 +109,13 @@ func ParseConfig(path string) (*Config, error) {
 	config := &Config{}
 	err = yaml.Unmarshal(content, &config)
 	return config, err
+}
+
+func NotIn(slice []string, value string) bool {
+	for _, item := range slice {
+		if item == value {
+			return false
+		}
+	}
+	return true
 }
--- config/config_test.go
+++ config/config_test.go
@@ -16,3 +16,17 @@ func TestParseConfig(t *testing.T) {
 		t.Errorf("Expected to get 1 application server, but got '%v'", len(config.Servers))
 	}
 }
+
+func TestGetTiers(t *testing.T) {
+	config, err := ParseConfig("test_files/tiers.yml")
+	if err != nil {
+		t.Errorf("Expected to not get error while parsing tiers.yml, but got %v", err)
+	}
+	tiers, err := config.GetTiers()
+	if err != nil {
+		t.Errorf("Expected to not get error while splitting config into tiers, but got %v", err)
+	}
+	if len(tiers) != 3 {
+		t.Errorf("Expected to get 3 tiers, but got %v", len(tiers))
+	}
+}
