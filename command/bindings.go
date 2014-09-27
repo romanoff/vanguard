2e632efed17c1cae7dc1d7fa2308b9412f9ec772
Andriy Romanov <judo.ras@gmail.com>
2014-09-26 17:19:03 -0700

    Added command to clear all bindings

--- command/bindings.go
+++ command/bindings.go
@@ -12,7 +12,15 @@ func NewBindingsCommand() cli.Command {
 		Usage: "shows port bindings",
 		Action: func(c *cli.Context) {
 			host := c.Args().First()
-			bindingsCommandFunc(host)
+			if host != "clear" {
+				bindingsCommandFunc(host)
+			} else {
+				host = ""
+				if len(c.Args()) == 2 {
+					host = c.Args()[1]
+				}
+				clearBindingsCommandFunc(host)
+			}
 		},
 	}
 }
@@ -29,5 +37,24 @@ func bindingsCommandFunc(host string) {
 	}
 	for _, binding := range bindings {
 		fmt.Println(host + " port " + binding.String())
+	}
+}
+
+func clearBindingsCommandFunc(host string) {
+	if host == "" {
+		host = "127.0.0.1"
+	}
+	vClient := client.NewClient(host)
+	bindings, err := vClient.Bindings()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, binding := range bindings {
+		err := vClient.Hide(binding.Port, "", "")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 }
